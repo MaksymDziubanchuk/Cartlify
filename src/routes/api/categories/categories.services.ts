bdedd1c0dca5dd77c0561267a74201056da049f0
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2026-02-17 18:44:16 +0100

    fix import error

--- src/routes/api/categories/categories.services.ts
+++ src/routes/api/categories/categories.services.ts
@@ -1,6 +1,6 @@
 import { findAllCategories } from './services/findAllCategories.service.js';
 import { createCategory } from './services/create.service.js';
-import { updateCategory } from './services/Update.service.js';
+import { updateCategory } from './services/update.service.js';
 import { deleteCategoryById } from './services/delete.service.js';
 
 export const categoriesServices = {
