1f84061224936de73d379fe6a2aacae331ceba02
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2026-04-13 19:39:47 +0200

    chat router structure fix

--- src/routes/api/chat/chat.routes.ts
+++ src/routes/api/chat/chat.routes.ts
@@ -4,16 +4,16 @@ import requireRole from '@middlewares/requireRole.js';
 import { chatsSchemas } from './chat.schemas.js';
 
 export default async function chatRouter(app: FastifyInstance, opt: unknown) {
-  // app.get(
-  //   '/',
-  //   {
-  //     preHandler: [authGuard, requireRole(['GUEST', 'ADMIN', 'ROOT', 'USER'])],
-  //     schema: chatsSchemas.getChatsSchema,
-  //   },
-  //   async () => {
-  //     return {
-  //       message: 'chat not implemented',
-  //     };
-  //   },
-  // );
+  app.get(
+    '/',
+    {
+      preHandler: [authGuard, requireRole(['GUEST', 'ADMIN', 'ROOT', 'USER'])],
+      schema: chatsSchemas.getChatsSchema,
+    },
+    async () => {
+      return {
+        message: 'chat not implemented',
+      };
+    },
+  );
 }
