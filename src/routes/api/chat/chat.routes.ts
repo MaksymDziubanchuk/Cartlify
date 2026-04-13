aca87890d2963b7d15d25ff89ec5a5a2ffcb04a2
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2026-04-13 19:53:26 +0200

    add preview for chat router

--- src/routes/api/chat/chat.routes.ts
+++ src/routes/api/chat/chat.routes.ts
@@ -4,16 +4,64 @@ import requireRole from '@middlewares/requireRole.js';
 import { chatsSchemas } from './chat.schemas.js';
 
 export default async function chatRouter(app: FastifyInstance, opt: unknown) {
-  app.get(
-    '/',
-    {
-      preHandler: [authGuard, requireRole(['GUEST', 'ADMIN', 'ROOT', 'USER'])],
-      schema: chatsSchemas.getChatsSchema,
-    },
-    async () => {
-      return {
-        message: 'chat not implemented',
-      };
-    },
-  );
+  // app.get(
+  //   '/threads',
+  //   {
+  //     preHandler: [authGuard, requireRole(['GUEST', 'USER', 'ADMIN', 'ROOT'])],
+  //     schema: chatsSchemas.getChatsSchema,
+  //   },
+  //   async () => {
+  //     return {
+  //       message: 'get chat threads not implemented',
+  //     };
+  //   },
+  // );
+  // app.get(
+  //   '/threads/:threadId/messages',
+  //   {
+  //     preHandler: [authGuard, requireRole(['GUEST', 'USER', 'ADMIN', 'ROOT'])],
+  //     schema: chatsSchemas.getChatMessagesSchema,
+  //   },
+  //   async () => {
+  //     return {
+  //       message: 'get chat messages not implemented',
+  //     };
+  //   },
+  // );
+  // app.post(
+  //   '/threads',
+  //   {
+  //     preHandler: [authGuard, requireRole(['GUEST', 'USER', 'ADMIN', 'ROOT'])],
+  //     schema: chatsSchemas.createChatThreadSchema,
+  //   },
+  //   async () => {
+  //     return {
+  //       message: 'create chat thread not implemented',
+  //     };
+  //   },
+  // );
+  // app.post(
+  //   '/threads/:threadId/messages',
+  //   {
+  //     preHandler: [authGuard, requireRole(['GUEST', 'USER', 'ADMIN', 'ROOT'])],
+  //     schema: chatsSchemas.createChatMessageSchema,
+  //   },
+  //   async () => {
+  //     return {
+  //       message: 'create chat message not implemented',
+  //     };
+  //   },
+  // );
+  // app.patch(
+  //   '/threads/:threadId/read',
+  //   {
+  //     preHandler: [authGuard, requireRole(['GUEST', 'USER', 'ADMIN', 'ROOT'])],
+  //     schema: chatsSchemas.markChatThreadReadSchema,
+  //   },
+  //   async () => {
+  //     return {
+  //       message: 'mark chat thread as read not implemented',
+  //     };
+  //   },
+  // );
 }
