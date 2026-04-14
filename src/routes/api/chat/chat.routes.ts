4e3690511857400933093a86387694107a17467c
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2026-04-14 17:43:25 +0200

    add get threads route

--- src/routes/api/chat/chat.routes.ts
+++ src/routes/api/chat/chat.routes.ts
@@ -4,18 +4,18 @@ import requireRole from '@middlewares/requireRole.js';
 import { chatsSchemas } from './chat.schemas.js';
 
 export default async function chatRouter(app: FastifyInstance, opt: unknown) {
-  // app.get(
-  //   '/threads',
-  //   {
-  //     preHandler: [authGuard, requireRole(['GUEST', 'USER', 'ADMIN', 'ROOT'])],
-  //     schema: chatsSchemas.getChatsSchema,
-  //   },
-  //   async () => {
-  //     return {
-  //       message: 'get chat threads not implemented',
-  //     };
-  //   },
-  // );
+  app.get(
+    '/threads',
+    {
+      preHandler: [authGuard, requireRole(['GUEST', 'USER', 'ADMIN', 'ROOT'])],
+      schema: chatsSchemas.getChatsSchema,
+    },
+    async () => {
+      return {
+        message: 'get chat threads not implemented',
+      };
+    },
+  );
   // app.get(
   //   '/threads/:threadId/messages',
   //   {
